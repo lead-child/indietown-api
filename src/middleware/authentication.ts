715ddac1eb0d1e4a3801eb184254d1b64efa5d65
Jake <jake@daangn.com>
2022-11-15 10:05:35 +0900

    feat: 인증 컨테스트에 userId 추가

--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -1,6 +1,7 @@
 import { verify } from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 import { UnauthroizeException } from "../common/exception";
+import prisma from "@src/prisma";
 
 export const authenticator = async (
   req: Request,
@@ -18,16 +19,35 @@ export const authenticator = async (
 
   verify(token, process.env.ACCESS_TOKEN_SECRET!!, async (err, decoded) => {
     if (err || !decoded || typeof decoded === "string") {
-      throw new UnauthroizeException(
-        "auth.invalid_auth_token",
-        "인증 토큰이 올바르지 않습니다."
+      next(
+        new UnauthroizeException(
+          "auth.invalid_auth_token",
+          "인증 토큰이 올바르지 않습니다."
+        )
       );
+      return;
     }
 
     const accountId = Number.parseInt(decoded.accountId, 10);
 
+    const user = await prisma.user.findFirst({
+      select: { id: true },
+      where: { accountId: { equals: accountId } },
+    });
+
+    if (!user) {
+      next(
+        new UnauthroizeException(
+          "auth.invalid_auth_token",
+          "인증 토큰이 올바르지 않습니다."
+        )
+      );
+      return;
+    }
+
     req.context = {
       accountId,
+      userId: user.id,
     };
 
     next();
--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -4,6 +4,7 @@ declare module "express-serve-static-core" {
   interface Request {
     context?: {
       accountId: number;
+      userId: number;
     };
   }
 }
